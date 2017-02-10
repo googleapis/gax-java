2b484b9807410efd866e9299e4c3e839e6a8bf0c
Shin Fan <shinfan@google.com>
2017-02-10 10:56:03 -0800

    Update test

--- src/test/java/com/google/api/gax/grpc/InstantiatingChannelProviderTest.java
+++ src/test/java/com/google/api/gax/grpc/InstantiatingChannelProviderTest.java
@@ -42,7 +42,7 @@ public class InstantiatingChannelProviderTest {
   @Test
   public void testServiceHeaderDefault() {
     InstantiatingChannelProvider provider = InstantiatingChannelProvider.newBuilder().build();
-    String expectedHeaderPattern = "^gl-java/.* gapic/(\\d+\\.?)+ gax/.* grpc/.*$";
+    String expectedHeaderPattern = "^gl-java/.* gapic/UNKNOWN gax/.* grpc/.*$";
     assertTrue(Pattern.compile(expectedHeaderPattern).matcher(provider.serviceHeader()).find());
   }
 
@@ -50,7 +50,9 @@ public class InstantiatingChannelProviderTest {
   public void testServiceHeaderCustomClient() {
     InstantiatingChannelProvider provider =
         InstantiatingChannelProvider.newBuilder().setClientLibHeader("gccl", "0.0.0").build();
-    String expectedHeaderPattern = "^gl-java/.* gccl/0\\.0\\.0 gapic/(\\d+\\.?)+ gax/.* grpc/.*$";
+    System.out.println(provider.serviceHeader());
+
+    String expectedHeaderPattern = "^gl-java/.* gccl/0\\.0\\.0 gapic/UNKNOWN gax/.* grpc/.*$";
     assertTrue(Pattern.compile(expectedHeaderPattern).matcher(provider.serviceHeader()).find());
   }
 
