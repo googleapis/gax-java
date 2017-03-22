d5509d5a6c46134c3e5183e9eb6cafd46418ae99
Shin Fan <shinfan@google.com>
2017-03-22 14:00:45 -0700

    Fix bad merge

--- src/test/java/com/google/api/gax/core/GaxPropertiesProviderTest.java
+++ src/test/java/com/google/api/gax/core/GaxPropertiesProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, Google Inc. All rights reserved.
+ * Copyright 2017, Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -37,7 +37,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class PropertiesProviderTest {
+public class GaxPropertiesProviderTest {
 
   @Test
   public void testGaxVersion() throws Exception {
