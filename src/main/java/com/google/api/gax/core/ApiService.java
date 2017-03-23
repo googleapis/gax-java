286e56cbe9ecf51e49ade605c65cb9416fbb464f
Michael Darakananda <pongad@google.com>
2017-03-23 14:09:35 +1100

    pr comment

--- src/main/java/com/google/api/gax/core/ApiService.java
+++ src/main/java/com/google/api/gax/core/ApiService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, Google Inc. All rights reserved.
+ * Copyright 2017, Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -29,15 +29,41 @@
  */
 package com.google.api.gax.core;
 
+import com.google.protobuf.ExperimentalApi;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
- * An object with an operational state. This is an unstable API.
+ * An object with an operational state, plus asynchronous {@link #startAsync()} and
+ * {@link #stopAsync()} lifecycle methods to transition between states. Example services include
+ * webservers, RPC servers and timers.
+ *
+ * <p>
+ * The normal lifecycle of a service is:
+ * <ul>
+ * <li>{@linkplain State#NEW NEW} -&gt;
+ * <li>{@linkplain State#STARTING STARTING} -&gt;
+ * <li>{@linkplain State#RUNNING RUNNING} -&gt;
+ * <li>{@linkplain State#STOPPING STOPPING} -&gt;
+ * <li>{@linkplain State#TERMINATED TERMINATED}
+ * </ul>
+ *
+ * <p>
+ * There are deviations from this if there are failures or if {@link ApiService#stopAsync} is called
+ * before the {@link ApiService} reaches the {@linkplain State#RUNNING RUNNING} state. The set of
+ * legal transitions form a <a href="http://en.wikipedia.org/wiki/Directed_acyclic_graph">DAG</a>,
+ * therefore every method of the listener will be called at most once. N.B. The {@link State#FAILED}
+ * and {@link State#TERMINATED} states are terminal states, once a service enters either of these
+ * states it cannot ever leave them.
+ *
+ * <p>
+ * Implementors of this interface are strongly encouraged to extend one of the abstract classes in
+ * this package which implement this interface and make the threading and state management easier.
  *
  * Similar to Guava's {@code Service}, but redeclared so that Guava could be shaded.
  */
+@ExperimentalApi
 public interface ApiService {
   void addListener(Listener listener, Executor executor);
 
