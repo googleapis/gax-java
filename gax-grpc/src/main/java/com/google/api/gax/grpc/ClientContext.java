googleapis/gax-java

commit 5830ace737628d211a673af986011e03e97f6dc1

Move CredentialProvider up to ClientSettings (#305)
Updates GoogleCloudPlatform/google-cloud-java#2034.

This commit should resolve the first 3 bullets in the
linked issue.

Major changes are:
- ClientContext is added to pull together channel, executor, and credentials,
  which are often needed together.
  This change will also simplify future work if more ClientSettings are added.

gax-grpc/src/main/java/com/google/api/gax/grpc/ClientContext.java
@@ -1,0 +1,116 @@
+/*
+ * Copyright 2017, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.google.api.gax.grpc;
+
+import com.google.api.core.BetaApi;
+import com.google.auth.Credentials;
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.concurrent.ScheduledExecutorService;
+import javax.annotation.Nullable;
+
+/**
+ * Encapsulates client state, including channel, executor, and credentials.
+ *
+ * <p>Unlike {@link ClientSettings} which allows users to configure the client, {@code
+ * ClientContext} is intended to be used in generated code. Most users will not need to use it.
+ */
+@BetaApi
+@AutoValue
+public abstract class ClientContext {
+  public abstract Collection<AutoCloseable> getCloseables();
+
+  public abstract Channel getChannel();
+
+  public abstract ScheduledExecutorService getExecutor();
+
+  @Nullable
+  public abstract Credentials getCredentials();
+
+  static Builder newBuilder() {
+    return new AutoValue_ClientContext.Builder();
+  }
+
+  public static ClientContext create(ClientSettings settings) throws IOException {
+    ImmutableList.Builder<AutoCloseable> closeables = ImmutableList.builder();
+
+    ExecutorProvider executorProvider = settings.getExecutorProvider();
+    final ScheduledExecutorService executor = executorProvider.getExecutor();
+    if (executorProvider.shouldAutoClose()) {
+      closeables.add(
+          new AutoCloseable() {
+            @Override
+            public void close() {
+              executor.shutdown();
+            }
+          });
+    }
+
+    final ManagedChannel channel;
+    ChannelProvider channelProvider = settings.getChannelProvider();
+    if (channelProvider.needsExecutor()) {
+      channel = channelProvider.getChannel(executor);
+    } else {
+      channel = channelProvider.getChannel();
+    }
+    if (channelProvider.shouldAutoClose()) {
+      closeables.add(
+          new AutoCloseable() {
+            @Override
+            public void close() {
+              channel.shutdown();
+            }
+          });
+    }
+    return newBuilder()
+        .setCloseables(closeables.build())
+        .setChannel(channel)
+        .setExecutor(executor)
+        .setCredentials(settings.getCredentialsProvider().getCredentials())
+        .build();
+  }
+
+  @AutoValue.Builder
+  abstract static class Builder {
+    public abstract Builder setCloseables(Collection<AutoCloseable> value);
+
+    public abstract Builder setChannel(Channel value);
+
+    public abstract Builder setExecutor(ScheduledExecutorService value);
+
+    public abstract Builder setCredentials(Credentials value);
+
+    public abstract ClientContext build();
+  }
+}

gax-grpc/src/main/java/com/google/api/gax/grpc/AuthCallable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, Google Inc. All rights reserved.
+ * Copyright 2017, Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -29,44 +29,31 @@
  */
 package com.google.api.gax.grpc;
 
-import com.google.api.core.BetaApi;
-import com.google.auto.value.AutoValue;
-import io.grpc.ManagedChannel;
-import java.io.IOException;
-import java.util.concurrent.ScheduledExecutorService;
+import com.google.api.core.ApiFuture;
+import com.google.auth.Credentials;
+import com.google.common.base.Preconditions;
+import io.grpc.CallCredentials;
+import io.grpc.auth.MoreCallCredentials;
 
 /**
- * ChannelAndExecutor holds a ManagedChannel and a ScheduledExecutorService that are being provided
- * as a pair.
+ * Implements the credential insertion for {@link UnaryCallable}.
+ *
+ * <p>Package-private for internal use.
  */
-@BetaApi
-@AutoValue
-@Deprecated
-public abstract class ChannelAndExecutor {
-  public abstract ScheduledExecutorService getExecutor();
+class AuthCallable<RequestT, ResponseT> implements FutureCallable<RequestT, ResponseT> {
+  private final FutureCallable<RequestT, ResponseT> callable;
+  private final CallCredentials credentials;
 
-  public abstract ManagedChannel getChannel();
-
-  /** Creates a ChannelAndExecutor simply containing the given channel and executor. */
-  public static ChannelAndExecutor create(
-      ScheduledExecutorService executor, ManagedChannel channel) {
-    return new AutoValue_ChannelAndExecutor(executor, channel);
+  AuthCallable(FutureCallable<RequestT, ResponseT> callable, Credentials credentials) {
+    this.callable = Preconditions.checkNotNull(callable);
+    this.credentials = MoreCallCredentials.from(Preconditions.checkNotNull(credentials));
   }
 
-  /**
-   * Creates an executor using the given ExecutorProvider and a channel using the given
-   * ChannelProvider, providing the executor to the channel if the channel needs an executor, and
-   * then returns a ChannelAndExecutor containing both.
-   */
-  public static ChannelAndExecutor create(
-      ExecutorProvider executorProvider, ChannelProvider channelProvider) throws IOException {
-    ScheduledExecutorService executor = executorProvider.getExecutor();
-    ManagedChannel channel = null;
-    if (channelProvider.needsExecutor()) {
-      channel = channelProvider.getChannel(executor);
-    } else {
-      channel = channelProvider.getChannel();
+  @Override
+  public ApiFuture<ResponseT> futureCall(RequestT request, CallContext context) {
+    if (context.getCallOptions().getCredentials() == null) {
+      context = context.withCallOptions(context.getCallOptions().withCallCredentials(credentials));
     }
-    return ChannelAndExecutor.create(executor, channel);
+    return callable.futureCall(request, context);
   }
 }

gax-grpc/src/main/java/com/google/api/gax/grpc/BatchingCallSettings.java
@@ -34,11 +34,9 @@ import com.google.api.gax.batching.BatchingSettings;
 import com.google.api.gax.retrying.RetrySettings;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
-import io.grpc.Channel;
 import io.grpc.MethodDescriptor;
 import io.grpc.Status;
 import java.util.Set;
-import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * A settings class to configure a UnaryCallable for calls to an API method that supports batching.
@@ -52,9 +50,10 @@ public final class BatchingCallSettings<RequestT, ResponseT>
   private BatcherFactory<RequestT, ResponseT> batcherFactory;
 
   /** Package-private, for use by UnaryCallable. */
-  UnaryCallable<RequestT, ResponseT> create(Channel channel, ScheduledExecutorService executor) {
-    UnaryCallable<RequestT, ResponseT> baseCallable = createBaseCallable(channel, executor);
-    batcherFactory = new BatcherFactory<>(batchingDescriptor, batchingSettings, executor);
+  UnaryCallable<RequestT, ResponseT> create(ClientContext context) {
+    UnaryCallable<RequestT, ResponseT> baseCallable = createBaseCallable(context);
+    batcherFactory =
+        new BatcherFactory<>(batchingDescriptor, batchingSettings, context.getExecutor());
     return baseCallable.batching(batchingDescriptor, batcherFactory);
   }
 

gax-grpc/src/main/java/com/google/api/gax/grpc/ClientSettings.java
@@ -30,10 +30,14 @@
 package com.google.api.gax.grpc;
 
 import com.google.api.core.BetaApi;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
 import com.google.api.gax.retrying.RetrySettings;
+import com.google.common.base.MoreObjects;
 import io.grpc.Status;
 import java.io.IOException;
 import java.util.Set;
+import javax.annotation.Nullable;
 
 /**
  * A base settings class to configure a service API class.
@@ -65,14 +69,25 @@ public abstract class ClientSettings {
 
   private final ExecutorProvider executorProvider;
   private final ChannelProvider channelProvider;
+  @Nullable private final CredentialsProvider credentialsProvider;
+
+  @Deprecated
+  protected ClientSettings(ExecutorProvider executorProvider, ChannelProvider channelProvider) {
+    this(executorProvider, channelProvider, null);
+  }
 
   /** Constructs an instance of ClientSettings. */
-  protected ClientSettings(ExecutorProvider executorProvider, ChannelProvider channelProvider) {
+  protected ClientSettings(
+      ExecutorProvider executorProvider,
+      ChannelProvider channelProvider,
+      CredentialsProvider credentialsProvider) {
     this.executorProvider = executorProvider;
     this.channelProvider = channelProvider;
+    this.credentialsProvider = credentialsProvider;
   }
 
   /** Gets a channel and an executor for making calls. */
+  @Deprecated
   public final ChannelAndExecutor getChannelAndExecutor() throws IOException {
     return ChannelAndExecutor.create(executorProvider, channelProvider);
   }
@@ -85,20 +100,36 @@ public abstract class ClientSettings {
     return channelProvider;
   }
 
+  @Nullable
+  public final CredentialsProvider getCredentialsProvider() {
+    return credentialsProvider;
+  }
+
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("executorProvider", executorProvider)
+        .add("channelProvider", channelProvider)
+        .add("credentialsProvider", credentialsProvider)
+        .toString();
+  }
+
   public abstract static class Builder {
 
     private ExecutorProvider executorProvider;
     private ChannelProvider channelProvider;
+    private CredentialsProvider credentialsProvider;
 
     /** Create a builder from a ClientSettings object. */
     protected Builder(ClientSettings settings) {
       this.executorProvider = settings.executorProvider;
       this.channelProvider = settings.channelProvider;
+      this.credentialsProvider = settings.credentialsProvider;
     }
 
     protected Builder(InstantiatingChannelProvider channelProvider) {
       this.executorProvider = InstantiatingExecutorProvider.newBuilder().build();
       this.channelProvider = channelProvider;
+      this.credentialsProvider = new NoCredentialsProvider();
     }
 
     /**
@@ -118,6 +149,12 @@ public abstract class ClientSettings {
       return this;
     }
 
+    /** Sets the CredentialsProvider to use for getting the channel to make calls with. */
+    public Builder setCredentialsProvider(CredentialsProvider credentialsProvider) {
+      this.credentialsProvider = credentialsProvider;
+      return this;
+    }
+
     /** Gets the ExecutorProvider that was previously set on this Builder. */
     public ExecutorProvider getExecutorProvider() {
       return executorProvider;
@@ -126,6 +163,11 @@ public abstract class ClientSettings {
     /** Gets the ChannelProvider that was previously set on this Builder. */
     public ChannelProvider getChannelProvider() {
       return channelProvider;
+    }
+
+    /** Gets the CredentialsProvider that was previously set on this Builder. */
+    public CredentialsProvider getCredentialsProvider() {
+      return credentialsProvider;
     }
 
     /** Performs a merge, using only non-null fields */
@@ -148,5 +190,13 @@ public abstract class ClientSettings {
     }
 
     public abstract ClientSettings build() throws IOException;
+
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("executorProvider", executorProvider)
+          .add("channelProvider", channelProvider)
+          .add("credentialsProvider", credentialsProvider)
+          .toString();
+    }
   }
 }

gax-grpc/src/main/java/com/google/api/gax/grpc/InstantiatingChannelProvider.java
@@ -139,6 +139,7 @@ public final class InstantiatingChannelProvider implements ChannelProvider {
    * Gets the credentials which will be used to call the service. If the credentials have not been
    * acquired yet, then they will be acquired when this function is called.
    */
+  @Deprecated
   public Credentials getCredentials() throws IOException {
     return getCredentialsProvider().getCredentials();
   }
@@ -147,6 +148,7 @@ public final class InstantiatingChannelProvider implements ChannelProvider {
    * The credentials to use in order to call the service. Credentials will not be acquired until
    * they are required.
    */
+  @Deprecated
   public CredentialsProvider getCredentialsProvider() {
     return credentialsProvider;
   }
@@ -261,12 +263,14 @@ public final class InstantiatingChannelProvider implements ChannelProvider {
      * Sets the CredentialsProvider which will acquire the credentials for making calls to the
      * service. Credentials will not be acquired until they are required.
      */
+    @Deprecated
     public Builder setCredentialsProvider(CredentialsProvider credentialsProvider) {
       this.credentialsProvider = credentialsProvider;
       return this;
     }
 
     /** The previously set CredentialsProvider. */
+    @Deprecated
     public CredentialsProvider getCredentialsProvider() {
       return credentialsProvider;
     }

gax-grpc/src/main/java/com/google/api/gax/grpc/OperationCallSettings.java
@@ -33,9 +33,7 @@ import com.google.api.core.BetaApi;
 import com.google.longrunning.Operation;
 import com.google.longrunning.OperationsClient;
 import com.google.protobuf.Message;
-import io.grpc.Channel;
 import io.grpc.MethodDescriptor;
-import java.util.concurrent.ScheduledExecutorService;
 import org.threeten.bp.Duration;
 
 /**
@@ -59,12 +57,10 @@ public final class OperationCallSettings<RequestT, ResponseT extends Message> {
 
   // package-private for internal use.
   OperationCallable<RequestT, ResponseT> createOperationCallable(
-      Channel channel, ScheduledExecutorService executor, OperationsClient operationsClient) {
-    UnaryCallable<RequestT, Operation> initialCallable =
-        initialCallSettings.create(channel, executor);
+      ClientContext context, OperationsClient operationsClient) {
+    UnaryCallable<RequestT, Operation> initialCallable = initialCallSettings.create(context);
     OperationCallable<RequestT, ResponseT> operationCallable =
-        new OperationCallable<>(
-            initialCallable, channel, executor, operationsClient, responseClass, this);
+        new OperationCallable<>(initialCallable, context, operationsClient, responseClass, this);
     return operationCallable;
   }
 

gax-grpc/src/main/java/com/google/api/gax/grpc/OperationCallable.java
@@ -48,8 +48,7 @@ import org.threeten.bp.Duration;
 @BetaApi
 public final class OperationCallable<RequestT, ResponseT extends Message> {
   private final UnaryCallable<RequestT, Operation> initialCallable;
-  private final Channel channel;
-  private final ScheduledExecutorService executor;
+  private final ClientContext clientContext;
   private final OperationsClient operationsClient;
   private final Class<ResponseT> responseClass;
   private final OperationCallSettings settings;
@@ -57,28 +56,15 @@ public final class OperationCallable<RequestT, ResponseT extends Message> {
   /** Package-private for internal use. */
   OperationCallable(
       UnaryCallable<RequestT, Operation> initialCallable,
-      Channel channel,
-      ScheduledExecutorService executor,
+      ClientContext clientContext,
       OperationsClient operationsClient,
       Class<ResponseT> responseClass,
       OperationCallSettings settings) {
     this.initialCallable = Preconditions.checkNotNull(initialCallable);
-    this.channel = channel;
-    this.executor = executor;
+    this.clientContext = clientContext;
     this.operationsClient = operationsClient;
     this.responseClass = responseClass;
     this.settings = settings;
-  }
-
-  /**
-   * Create an OperationCallable with a bound channel. If a call is made without specifying a
-   * channel, the {@code boundChannel} is used instead.
-   *
-   * <p>Package-private for internal use.
-   */
-  OperationCallable<RequestT, ResponseT> bind(Channel boundChannel) {
-    return new OperationCallable<>(
-        initialCallable, boundChannel, executor, operationsClient, responseClass, settings);
   }
 
   /**
@@ -87,23 +73,32 @@ public final class OperationCallable<RequestT, ResponseT extends Message> {
    *
    * @param operationCallSettings {@link com.google.api.gax.grpc.OperationCallSettings} to configure
    *     the method-level settings with.
-   * @param channel {@link Channel} to use to connect to the service.
-   * @param executor {@link ScheduledExecutorService} to use to schedule polling work.
+   * @param clientContext {@link ClientContext} to use to connect to the service.
    * @param operationsClient {@link OperationsClient} to use to poll for updates on the Operation.
    * @return {@link com.google.api.gax.grpc.OperationCallable} callable object.
    */
   public static <RequestT, ResponseT extends Message> OperationCallable<RequestT, ResponseT> create(
       OperationCallSettings<RequestT, ResponseT> operationCallSettings,
+      ClientContext clientContext,
+      OperationsClient operationsClient) {
+    return operationCallSettings.createOperationCallable(clientContext, operationsClient);
+  }
+
+  @Deprecated
+  public static <RequestT, ResponseT extends Message> OperationCallable<RequestT, ResponseT> create(
+      OperationCallSettings<RequestT, ResponseT> operationCallSettings,
       Channel channel,
       ScheduledExecutorService executor,
       OperationsClient operationsClient) {
-    return operationCallSettings.createOperationCallable(channel, executor, operationsClient);
+    return operationCallSettings.createOperationCallable(
+        ClientContext.newBuilder().setChannel(channel).setExecutor(executor).build(),
+        operationsClient);
   }
 
   /**
    * Initiates an operation asynchronously. If the {@link io.grpc.Channel} encapsulated in the given
-   * {@link com.google.api.gax.grpc.CallContext} is null, a channel must have already been bound
-   * either at construction time or using {@link #bind(Channel)}.
+   * {@link com.google.api.gax.grpc.CallContext} is null, a channel must have already been bound at
+   * construction time.
    *
    * @param request The request to initiate the operation.
    * @param context {@link com.google.api.gax.grpc.CallContext} to make the call with
@@ -111,14 +106,18 @@ public final class OperationCallable<RequestT, ResponseT extends Message> {
    */
   public OperationFuture<ResponseT> futureCall(RequestT request, CallContext context) {
     if (context.getChannel() == null) {
-      context = context.withChannel(channel);
+      context = context.withChannel(clientContext.getChannel());
     }
     ApiFuture<Operation> initialCallFuture = initialCallable.futureCall(request, context);
     Duration pollingInterval =
         settings != null ? settings.getPollingInterval() : OperationFuture.DEFAULT_POLLING_INTERVAL;
     OperationFuture<ResponseT> operationFuture =
         OperationFuture.create(
-            operationsClient, initialCallFuture, executor, responseClass, pollingInterval);
+            operationsClient,
+            initialCallFuture,
+            clientContext.getExecutor(),
+            responseClass,
+            pollingInterval);
     return operationFuture;
   }
 
@@ -130,13 +129,13 @@ public final class OperationCallable<RequestT, ResponseT extends Message> {
    * @return {@link OperationFuture} for the call result
    */
   public OperationFuture<ResponseT> futureCall(RequestT request) {
-    return futureCall(request, CallContext.createDefault().withChannel(channel));
+    return futureCall(request, CallContext.createDefault().withChannel(clientContext.getChannel()));
   }
 
   /**
    * Initiates an operation and polls for the final result. If the {@link io.grpc.Channel}
    * encapsulated in the given {@link com.google.api.gax.grpc.CallContext} is null, a channel must
-   * have already been bound, using {@link #bind(Channel)}.
+   * have already been bound at construction time.
    *
    * @param request The request to initiate the operation.
    * @param context {@link com.google.api.gax.grpc.CallContext} to make the call with
@@ -175,7 +174,8 @@ public final class OperationCallable<RequestT, ResponseT extends Message> {
             .getOperationCallable()
             .futureCall(GetOperationRequest.newBuilder().setName(operationName).build());
     OperationFuture<ResponseT> operationFuture =
-        OperationFuture.create(operationsClient, getOperationFuture, executor, responseClass);
+        OperationFuture.create(
+            operationsClient, getOperationFuture, clientContext.getExecutor(), responseClass);
     return operationFuture;
   }
 }

gax-grpc/src/main/java/com/google/api/gax/grpc/PagedCallSettings.java
@@ -32,11 +32,9 @@ package com.google.api.gax.grpc;
 import com.google.api.core.BetaApi;
 import com.google.api.gax.retrying.RetrySettings;
 import com.google.common.collect.ImmutableSet;
-import io.grpc.Channel;
 import io.grpc.MethodDescriptor;
 import io.grpc.Status;
 import java.util.Set;
-import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * A settings class to configure a UnaryCallable for calls to an API method that supports page
@@ -49,14 +47,13 @@ public final class PagedCallSettings<RequestT, ResponseT, PagedListResponseT>
       pagedListResponseFactory;
 
   /** Package-private, for use by UnaryCallable. */
-  UnaryCallable<RequestT, ResponseT> create(Channel channel, ScheduledExecutorService executor) {
-    return createBaseCallable(channel, executor);
+  UnaryCallable<RequestT, ResponseT> create(ClientContext context) {
+    return createBaseCallable(context);
   }
 
   /** Package-private, for use by UnaryCallable. */
-  UnaryCallable<RequestT, PagedListResponseT> createPagedVariant(
-      Channel channel, ScheduledExecutorService executor) {
-    UnaryCallable<RequestT, ResponseT> baseCallable = createBaseCallable(channel, executor);
+  UnaryCallable<RequestT, PagedListResponseT> createPagedVariant(ClientContext context) {
+    UnaryCallable<RequestT, ResponseT> baseCallable = createBaseCallable(context);
     return baseCallable.paged(pagedListResponseFactory);
   }
 

commit 3c67e25e4834fd7f6ffc274f4d49645b207a5f43

make ClientContext work properly with old surface
This fixes a bug introduced in #305.
UnaryCallable uses ClientContext to pass values.
To ease transition, old surfaces are marked deprecated
but reimplemented using ClientContext.

These old surfaces did not populate closeables,
so ClientContext cannot be initialized.
This PR side steps this problem by initializing closeables
to empty list.

gax-grpc/src/main/java/com/google/api/gax/grpc/ClientContext.java
@@ -37,6 +37,7 @@ import io.grpc.Channel;
 import io.grpc.ManagedChannel;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.ScheduledExecutorService;
 import javax.annotation.Nullable;
 
@@ -59,7 +60,8 @@ public abstract class ClientContext {
   public abstract Credentials getCredentials();
 
   static Builder newBuilder() {
-    return new AutoValue_ClientContext.Builder();
+    return new AutoValue_ClientContext.Builder()
+        .setCloseables(Collections.<AutoCloseable>emptyList());
   }
 
   public static ClientContext create(ClientSettings settings) throws IOException {