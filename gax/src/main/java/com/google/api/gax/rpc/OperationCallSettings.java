fffcf4409e9b58bc83d278f4b9b6f4825f572f53
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2017-06-16 12:32:47 -0700

    Adding transport-agnostic abstraction

---
+++ gax/src/main/java/com/google/api/gax/rpc/OperationCallSettings.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2016, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.google.api.gax.rpc;
+
+import com.google.api.core.BetaApi;
+import org.threeten.bp.Duration;
+
+/** A settings class to configure an OperationCallable for calls to a long-running API method. */
+@BetaApi
+public final class OperationCallSettings<RequestT, ResponseT, OperationT> {
+  static final Duration DEFAULT_POLLING_INTERVAL = Duration.ofSeconds(1);
+
+  private final SimpleCallSettings<RequestT, OperationT> initialCallSettings;
+  private final Class<ResponseT> responseClass;
+  private final Duration pollingInterval;
+
+  public final SimpleCallSettings<RequestT, OperationT> getInitialCallSettings() {
+    return initialCallSettings;
+  }
+
+  public Class<ResponseT> getResponseClass() {
+    return responseClass;
+  }
+
+  public final Duration getPollingInterval() {
+    return pollingInterval;
+  }
+
+  private OperationCallSettings(
+      SimpleCallSettings<RequestT, OperationT> initialCallSettings,
+      Class<ResponseT> responseClass,
+      Duration pollingInterval) {
+    this.initialCallSettings = initialCallSettings;
+    this.responseClass = responseClass;
+    this.pollingInterval = pollingInterval;
+  }
+
+  /** Create a new builder which can construct an instance of OperationCallSettings. */
+  public static <RequestT, ResponseT, OperationT>
+      Builder<RequestT, ResponseT, OperationT> newBuilder(Class<ResponseT> responseClass) {
+    return new Builder<>(responseClass);
+  }
+
+  public final Builder<RequestT, ResponseT, OperationT> toBuilder() {
+    return new Builder<>(this);
+  }
+
+  public static class Builder<RequestT, ResponseT, OperationT> {
+    private SimpleCallSettings.Builder<RequestT, OperationT> initialCallSettings;
+    private Class<ResponseT> responseClass;
+    private Duration pollingInterval = DEFAULT_POLLING_INTERVAL;
+
+    public Builder(Class<ResponseT> responseClass) {
+      this.initialCallSettings = SimpleCallSettings.newBuilder();
+      this.responseClass = responseClass;
+    }
+
+    public Builder(OperationCallSettings<RequestT, ResponseT, OperationT> settings) {
+      this.initialCallSettings = settings.initialCallSettings.toBuilder();
+      this.responseClass = settings.responseClass;
+    }
+
+    /** Set the polling interval of the operation. */
+    public Builder setPollingInterval(Duration pollingInterval) {
+      this.pollingInterval = pollingInterval;
+      return this;
+    }
+
+    /** Get the polling interval of the operation. */
+    public Duration getPollingInterval() {
+      return pollingInterval;
+    }
+
+    /** Set the call settings which are used on the call to initiate the operation. */
+    public Builder setInitialCallSettings(
+        SimpleCallSettings.Builder<RequestT, OperationT> initialCallSettings) {
+      this.initialCallSettings = initialCallSettings;
+      return this;
+    }
+
+    /** Get the call settings which are used on the call to initiate the operation. */
+    public SimpleCallSettings.Builder<RequestT, OperationT> getInitialCallSettings() {
+      return initialCallSettings;
+    }
+
+    public OperationCallSettings<RequestT, ResponseT, OperationT> build() {
+      return new OperationCallSettings<>(
+          initialCallSettings.build(), responseClass, pollingInterval);
+    }
+  }
+}
---
+++ gax/src/test/java/com/google/api/gax/rpc/UnaryCallableTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2017, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.google.api.gax.rpc;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.common.truth.Truth;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+@RunWith(JUnit4.class)
+public class UnaryCallableTest {
+  private static class StashCallable<RequestT, ResponseT>
+      implements UnaryCallableImpl<RequestT, ResponseT> {
+    ApiCallContext context;
+    RequestT request;
+    ResponseT result;
+
+    public StashCallable(ResponseT result) {
+      this.result = result;
+    }
+
+    @Override
+    public ApiFuture<ResponseT> futureCall(RequestT request, ApiCallContext context) {
+      this.request = request;
+      this.context = context;
+      return ApiFutures.immediateFuture(result);
+    }
+  }
+
+  @Test
+  public void call() throws Exception {
+    StashCallable<Integer, Integer> stash = new StashCallable<>(1);
+
+    UnaryCallable<Integer, Integer> callable = UnaryCallable.create(stash);
+    Integer response = callable.call(2);
+    Truth.assertThat(response).isEqualTo(Integer.valueOf(1));
+    Truth.assertThat(stash.context).isNull();
+  }
+
+  @Test
+  public void callWithContext() throws Exception {
+    ApiCallContext context = Mockito.mock(ApiCallContext.class);
+    StashCallable<Integer, Integer> stash = new StashCallable<>(1);
+
+    UnaryCallable<Integer, Integer> callable = UnaryCallable.create(stash);
+    Integer response = callable.call(2, context);
+    Truth.assertThat(response).isEqualTo(Integer.valueOf(1));
+    Truth.assertThat(stash.context).isSameAs(context);
+  }
+
+  @Test
+  public void callWithCallContextDecorator() throws Exception {
+    final ApiCallContext outerContext = Mockito.mock(ApiCallContext.class);
+    ApiCallContextDecorator decorator =
+        new ApiCallContextDecorator() {
+          @Override
+          public ApiCallContext decorate(ApiCallContext context) {
+            return outerContext;
+          }
+        };
+    ApiCallContext context = Mockito.mock(ApiCallContext.class);
+    StashCallable<Integer, Integer> stash = new StashCallable<>(1);
+
+    UnaryCallable<Integer, Integer> callable = UnaryCallable.create(stash, decorator);
+    Integer response = callable.call(2);
+    Truth.assertThat(response).isEqualTo(Integer.valueOf(1));
+    Truth.assertThat(stash.context).isSameAs(outerContext);
+  }
+}
