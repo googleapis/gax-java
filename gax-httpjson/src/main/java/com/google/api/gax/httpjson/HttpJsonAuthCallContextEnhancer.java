e60987a556982aff2d4d36169837bcace7c25c97
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2017-08-22 09:51:56 -0700

    Bootstrapping gax-httpjson (#355)

---
+++ gax-httpjson/src/main/java/com/google/api/gax/httpjson/HttpJsonAuthCallContextEnhancer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.google.api.gax.httpjson;
+
+import com.google.api.gax.rpc.ApiCallContext;
+import com.google.api.gax.rpc.ApiCallContextEnhancer;
+import com.google.auth.Credentials;
+
+/* Package-private for internal use. */
+class HttpJsonAuthCallContextEnhancer implements ApiCallContextEnhancer {
+
+  private final Credentials credentials;
+
+  public HttpJsonAuthCallContextEnhancer(Credentials credentials) {
+    this.credentials = credentials;
+  }
+
+  @Override
+  public HttpJsonCallContext enhance(ApiCallContext inputContext) {
+    HttpJsonCallContext context =
+        HttpJsonCallContext.getAsHttpJsonCallContextWithDefault(inputContext);
+    if (context.getCallOptions().getCredentials() == null) {
+      context = context.withCallOptions(context.getCallOptions().withCredentials(credentials));
+    }
+    return context;
+  }
+}
