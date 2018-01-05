5b7b681576ca2fb3ac8a4b9adc2bed611ade4ef5
Igor Bernstein <igorbernstein@google.com>
2018-01-05 13:53:04 -0500

    tweak docs

--- gax/src/main/java/com/google/api/gax/rpc/AbstractResponseObserver.java
+++ gax/src/main/java/com/google/api/gax/rpc/AbstractResponseObserver.java
@@ -38,6 +38,12 @@ public abstract class AbstractResponseObserver<V> implements ResponseObserver<V>
   private boolean isStarted;
   private boolean isClosed;
 
+  /**
+   * {@inheritDoc}
+   *
+   * <p>This implementation simply delegates to {@link #onStartImpl(StreamController)} after
+   * ensuring consistent state.
+   */
   public final void onStart(StreamController controller) {
     Preconditions.checkState(!isStarted, getClass() + " is already started.");
     isStarted = true;
@@ -45,65 +51,50 @@ public abstract class AbstractResponseObserver<V> implements ResponseObserver<V>
     onStartImpl(controller);
   }
 
+  /**
+   * {@inheritDoc}
+   *
+   * <p>This implementation simply delegates to {@link #onResponse(Object)} after ensuring
+   * consistent state.
+   */
   public final void onResponse(V response) {
     Preconditions.checkState(!isClosed, getClass() + " received a response after being closed.");
     onResponseImpl(response);
   }
 
+  /**
+   * {@inheritDoc}
+   *
+   * <p>This implementation simply delegates to {@link #onComplete()} after ensuring consistent
+   * state.
+   */
   public final void onComplete() {
     Preconditions.checkState(!isClosed, getClass() + " tried to double close.");
     isClosed = true;
     onCompleteImpl();
   }
 
+  /**
+   * {@inheritDoc}
+   *
+   * <p>This implementation simply delegates to {@link #onError(Throwable)} after ensuring
+   * consistent state.
+   */
   public final void onError(Throwable t) {
     Preconditions.checkState(!isClosed, getClass() + " received error after being closed", t);
     isClosed = true;
     onErrorImpl(t);
   }
 
-  /**
-   * Called before the stream is started. This must be invoked synchronously on the same thread that
-   * called {@link ServerStreamingCallable#call(Object, ResponseObserver, ApiCallContext)}
-   *
-   * <p>Allows for disabling flow control and early stream termination via {@code StreamController}.
-   *
-   * @param controller The controller for the stream.
-   */
+  /** @see #onStart(StreamController) */
   protected abstract void onStartImpl(StreamController controller);
 
-  /**
-   * Receives a value from the stream.
-   *
-   * <p>Can be called many times but is never called after {@link #onError(Throwable)} or {@link
-   * #onComplete()} are called.
-   *
-   * <p>Clients may may receive 0 or more onResponse callbacks.
-   *
-   * <p>If an exception is thrown by an implementation the caller will terminate the stream by
-   * calling {@link #onError(Throwable)} with the caught exception as the cause.
-   *
-   * @param response the value passed to the stream
-   */
+  /** @see #onResponse(Object) */
   protected abstract void onResponseImpl(V response);
 
-  /**
-   * Receives a terminating error from the stream.
-   *
-   * <p>May only be called once, and if called, it must be the last method called. In particular, if
-   * an exception is thrown by an implementation of {@code onError}, no further calls to any method
-   * are allowed.
-   *
-   * @param t the error occurred on the stream
-   */
+  /** @see #onErrorImpl(Throwable) */
   protected abstract void onErrorImpl(Throwable t);
 
-  /**
-   * Receives a notification of successful stream completion.
-   *
-   * <p>May only be called once, and if called, it must be the last method called. In particular, if
-   * an exception is thrown by an implementation of {@code onComplete}, no further calls to any
-   * method are allowed.
-   */
+  /** @see #onComplete() */
   protected abstract void onCompleteImpl();
 }
