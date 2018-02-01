ba63881bbc50a9f8e7238598b6fa101ff5b224b2
Andrea Lin <asdf.lina@gmail.com>
2018-02-01 13:33:21 -0800

    moving paging streaming insto Client

--- gax-grpc/src/main/java/com/google/longrunning/OperationsClient.java
+++ gax-grpc/src/main/java/com/google/longrunning/OperationsClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018, Google LLC All rights reserved.
+ * Copyright 2018 Google LLC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -106,6 +106,80 @@ import javax.annotation.Generated;
 public class OperationsClient implements BackgroundResource {
   private final OperationsSettings settings;
   private final OperationsStub stub;
+
+  public static class ListOperationsPagedResponse
+      extends AbstractPagedListResponse<
+          ListOperationsRequest, ListOperationsResponse, Operation, ListOperationsPage,
+          ListOperationsFixedSizeCollection> {
+
+    public static ApiFuture<ListOperationsPagedResponse> createAsync(
+        PageContext<ListOperationsRequest, ListOperationsResponse, Operation> context,
+        ApiFuture<ListOperationsResponse> futureResponse) {
+      ApiFuture<ListOperationsPage> futurePage =
+          ListOperationsPage.createEmptyPage().createPageAsync(context, futureResponse);
+      return ApiFutures.transform(
+          futurePage,
+          new ApiFunction<ListOperationsPage, ListOperationsPagedResponse>() {
+            @Override
+            public ListOperationsPagedResponse apply(ListOperationsPage input) {
+              return new ListOperationsPagedResponse(input);
+            }
+          });
+    }
+
+    private ListOperationsPagedResponse(ListOperationsPage page) {
+      super(page, ListOperationsFixedSizeCollection.createEmptyCollection());
+    }
+  }
+
+  public static class ListOperationsPage
+      extends AbstractPage<
+          ListOperationsRequest, ListOperationsResponse, Operation, ListOperationsPage> {
+
+    private ListOperationsPage(
+        PageContext<ListOperationsRequest, ListOperationsResponse, Operation> context,
+        ListOperationsResponse response) {
+      super(context, response);
+    }
+
+    private static ListOperationsPage createEmptyPage() {
+      return new ListOperationsPage(null, null);
+    }
+
+    @Override
+    protected ListOperationsPage createPage(
+        PageContext<ListOperationsRequest, ListOperationsResponse, Operation> context,
+        ListOperationsResponse response) {
+      return new ListOperationsPage(context, response);
+    }
+
+    @Override
+    public ApiFuture<ListOperationsPage> createPageAsync(
+        PageContext<ListOperationsRequest, ListOperationsResponse, Operation> context,
+        ApiFuture<ListOperationsResponse> futureResponse) {
+      return super.createPageAsync(context, futureResponse);
+    }
+  }
+
+  public static class ListOperationsFixedSizeCollection
+      extends AbstractFixedSizeCollection<
+          ListOperationsRequest, ListOperationsResponse, Operation, ListOperationsPage,
+          ListOperationsFixedSizeCollection> {
+
+    private ListOperationsFixedSizeCollection(List<ListOperationsPage> pages, int collectionSize) {
+      super(pages, collectionSize);
+    }
+
+    private static ListOperationsFixedSizeCollection createEmptyCollection() {
+      return new ListOperationsFixedSizeCollection(null, 0);
+    }
+
+    @Override
+    protected ListOperationsFixedSizeCollection createCollection(
+        List<ListOperationsPage> pages, int collectionSize) {
+      return new ListOperationsFixedSizeCollection(pages, collectionSize);
+    }
+  }
 
   /**
    * Constructs an instance of OperationsClient, using the given settings. The channels are created
