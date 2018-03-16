734f0d59055ccf6c538a9cb477b384822fbe62ad
Andrea Lin <asdf.lina@gmail.com>
2018-03-16 12:48:02 -0700

    MockHttpService sets type-specific response handlers

--- gax-httpjson/src/test/java/com/google/api/gax/httpjson/testing/MockHttpService.java
+++ gax-httpjson/src/test/java/com/google/api/gax/httpjson/testing/MockHttpService.java
@@ -39,7 +39,6 @@ import com.google.api.gax.httpjson.ApiMethodDescriptor;
 import com.google.api.pathtemplate.PathTemplate;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.LinkedList;
@@ -51,7 +50,7 @@ import java.util.Queue;
 public final class MockHttpService extends MockHttpTransport {
 
   private final List<String> requestPaths = new LinkedList<>();
-  private final Queue<Object> responses = new LinkedList<>();
+  private final Queue<HttpResponseFactory> responseHandlers = new LinkedList<>();
   private List<ApiMethodDescriptor<? extends ApiMessage, ? extends ApiMessage>> serializers;
   private String endpoint;
 
@@ -66,80 +65,80 @@ public final class MockHttpService extends MockHttpTransport {
     this.endpoint = fixedEndpoint;
   }
 
-  private static final class NullResponse {}
-
-  private MockLowLevelHttpResponse getHttpResponse(String targetUrl) {
-    MockLowLevelHttpResponse httpResponse = new MockLowLevelHttpResponse();
-    Preconditions.checkArgument(!responses.isEmpty());
-    Writer writer = new StringWriter();
-
-    Object response = responses.poll();
-    if (response instanceof ApiMessage) {
-      Preconditions.checkArgument(serializers != null, "MockHttpService has null serializers.");
-
-      // relativePath will be repeatedly truncated until it contains only
-      // the path template substring of the endpoint URL.
-      String relativePath = targetUrl.replaceFirst(endpoint, "");
-      int queryParamIndex = relativePath.indexOf("?");
-      queryParamIndex = queryParamIndex < 0 ? relativePath.length() : queryParamIndex;
-      relativePath = relativePath.substring(0, queryParamIndex);
-
-      for (ApiMethodDescriptor<? extends ApiMessage, ? extends ApiMessage> methodDescriptor :
-          serializers) {
-        // Server figures out which RPC method is called based on the endpoint path pattern.
-        if (PathTemplate.create(methodDescriptor.endpointPathTemplate()).matches(relativePath)) {
-          methodDescriptor.writeResponse(writer, response.getClass(), response);
-          httpResponse.setContent(writer.toString().getBytes());
-          httpResponse.setStatusCode(200);
-          return httpResponse;
-        }
-      }
-      throw new IllegalArgumentException("Method descriptor not found for response object.");
-    } else if (response instanceof Exception) {
-      Exception e = (Exception) response;
-      httpResponse.setStatusCode(400);
-      httpResponse.setContent(e.toString().getBytes());
-      httpResponse.setContentEncoding("text/plain");
-    } else if (response instanceof NullResponse) {
-      return new MockLowLevelHttpResponse().setStatusCode(200);
-    } else {
-      Exception e =
-          new IllegalArgumentException(
-              String.format(
-                  "response \"%s\" type \"%s\" is unrecognized",
-                  response.toString(), response.getClass().getName()));
-      httpResponse.setStatusCode(400);
-      httpResponse.setContent(e.toString().getBytes());
-      httpResponse.setContentEncoding("text/plain");
-    }
-
-    return httpResponse;
-  }
-
   @Override
-  public LowLevelHttpRequest buildRequest(String method, final String url) throws IOException {
+  public LowLevelHttpRequest buildRequest(String method, final String url) {
     requestPaths.add(url);
     return new MockLowLevelHttpRequest() {
       @Override
-      public LowLevelHttpResponse execute() throws IOException {
+      public LowLevelHttpResponse execute() {
         return getHttpResponse(url);
       }
     };
   }
 
   /* Add an ApiMessage to the response queue. */
-  public void addResponse(ApiMessage response) {
-    responses.add(response);
+  public void addResponse(final ApiMessage response) {
+    responseHandlers.add(
+        new HttpResponseFactory() {
+          @Override
+          public MockLowLevelHttpResponse getHttpResponse(String targetUrl) {
+            Writer writer = new StringWriter();
+            MockLowLevelHttpResponse httpResponse = new MockLowLevelHttpResponse();
+            Preconditions.checkArgument(
+                serializers != null, "MockHttpService has null serializers.");
+
+            // relativePath will be repeatedly truncated until it contains only
+            // the path template substring of the endpoint URL.
+            String relativePath = targetUrl.replaceFirst(endpoint, "");
+            int queryParamIndex = relativePath.indexOf("?");
+            queryParamIndex = queryParamIndex < 0 ? relativePath.length() : queryParamIndex;
+            relativePath = relativePath.substring(0, queryParamIndex);
+
+            for (ApiMethodDescriptor<? extends ApiMessage, ? extends ApiMessage> methodDescriptor :
+                serializers) {
+              // Server figures out which RPC method is called based on the endpoint path pattern.
+              if (PathTemplate.create(methodDescriptor.endpointPathTemplate())
+                  .matches(relativePath)) {
+                methodDescriptor.writeResponse(writer, response.getClass(), response);
+                httpResponse.setContent(writer.toString().getBytes());
+                httpResponse.setStatusCode(200);
+                return httpResponse;
+              }
+            }
+
+            // Return 404 when none of this server's endpoint templates match the given URL.
+            httpResponse.setContent(
+                String.format("Method not found for path '%s'", relativePath).getBytes());
+            httpResponse.setStatusCode(404);
+            return httpResponse;
+          }
+        });
   }
 
   /* Add an expected null response (empty HTTP response body). */
   public void addNullResponse() {
-    responses.add(new NullResponse());
+    responseHandlers.add(
+        new HttpResponseFactory() {
+          @Override
+          public MockLowLevelHttpResponse getHttpResponse(String targetUrl) {
+            return new MockLowLevelHttpResponse().setStatusCode(200);
+          }
+        });
   }
 
   /* Add an Exception to the response queue. */
-  public void addException(Exception exception) {
-    responses.add(exception);
+  public void addException(final Exception exception) {
+    responseHandlers.add(
+        new HttpResponseFactory() {
+          @Override
+          public MockLowLevelHttpResponse getHttpResponse(String targetUrl) {
+            MockLowLevelHttpResponse httpResponse = new MockLowLevelHttpResponse();
+            httpResponse.setStatusCode(400);
+            httpResponse.setContent(exception.toString().getBytes());
+            httpResponse.setContentEncoding("text/plain");
+            return httpResponse;
+          }
+        });
   }
 
   /* Get the FIFO list of URL paths to which requests were sent. */
@@ -149,7 +148,16 @@ public final class MockHttpService extends MockHttpTransport {
 
   /* Reset the expected response queue, the method descriptor, and the logged request paths list. */
   public void reset() {
-    responses.clear();
+    responseHandlers.clear();
     requestPaths.clear();
+  }
+
+  private MockLowLevelHttpResponse getHttpResponse(String targetUrl) {
+    Preconditions.checkArgument(!responseHandlers.isEmpty());
+    return responseHandlers.poll().getHttpResponse(targetUrl);
+  }
+
+  private interface HttpResponseFactory {
+    MockLowLevelHttpResponse getHttpResponse(String targetUrl);
   }
 }
