a00212b64f839e31f3d8d5d8add7fd6b8e520331
Andrea Lin <asdf.lina@gmail.com>
2018-03-23 16:49:16 -0700

    remove apimessage from httpjsoncallsettings

--- gax-httpjson/src/main/java/com/google/api/gax/httpjson/HttpJsonCallSettings.java
+++ gax-httpjson/src/main/java/com/google/api/gax/httpjson/HttpJsonCallSettings.java
@@ -41,12 +41,12 @@ public class HttpJsonCallSettings<RequestT, ResponseT> {
     return methodDescriptor;
   }
 
-  public static <RequestT extends ApiMessage, ResponseT extends ApiMessage>
+  public static <RequestT, ResponseT>
       Builder<RequestT, ResponseT> newBuilder() {
     return new Builder<>();
   }
 
-  public static <RequestT extends ApiMessage, ResponseT extends ApiMessage>
+  public static <RequestT, ResponseT>
       HttpJsonCallSettings<RequestT, ResponseT> create(
           ApiMethodDescriptor<RequestT, ResponseT> methodDescriptor) {
     return HttpJsonCallSettings.<RequestT, ResponseT>newBuilder()
