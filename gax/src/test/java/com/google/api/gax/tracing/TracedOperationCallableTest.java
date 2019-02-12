e9495f33a2791180e045d1ccb26b50368aef1100
Igor Bernstein <igorbernstein@google.com>
2019-02-12 13:13:48 -0500

    fix license

--- gax/src/test/java/com/google/api/gax/tracing/TracedOperationCallableTest.java
+++ gax/src/test/java/com/google/api/gax/tracing/TracedOperationCallableTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google LLC
+ * Copyright 2019 Google LLC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -26,7 +26,8 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ package com.google.api.gax.tracing;
+ */
+package com.google.api.gax.tracing;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
