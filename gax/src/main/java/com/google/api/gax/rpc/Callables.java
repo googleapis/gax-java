aade2f28eb6205c0d3710d3e7e86a67df2a41714
Andrea Lin <asdf.lina@gmail.com>
2019-05-13 12:54:01 -0700

    diff

--- gax/src/main/java/com/google/api/gax/rpc/Callables.java
+++ gax/src/main/java/com/google/api/gax/rpc/Callables.java
@@ -35,7 +35,6 @@ import com.google.api.gax.longrunning.OperationResponsePollAlgorithm;
 import com.google.api.gax.longrunning.OperationSnapshot;
 import com.google.api.gax.retrying.ExponentialRetryAlgorithm;
 import com.google.api.gax.retrying.RetryAlgorithm;
-import com.google.api.gax.retrying.RetrySettings;
 import com.google.api.gax.retrying.ScheduledRetryingExecutor;
 import com.google.api.gax.retrying.StreamingRetryAlgorithm;
 import java.util.Collection;
@@ -56,7 +55,7 @@ public class Callables {
       UnaryCallSettings<?, ?> callSettings,
       ClientContext clientContext) {
 
-    if (areRetriesDisabled(callSettings.getRetryableCodes(), callSettings.getRetrySettings())) {
+    if (areRetriesDisabled(callSettings.getRetryableCodes())) {
       return innerCallable;
     }
 
@@ -77,7 +76,7 @@ public class Callables {
       ServerStreamingCallSettings<RequestT, ResponseT> callSettings,
       ClientContext clientContext) {
 
-    if (areRetriesDisabled(callSettings.getRetryableCodes(), callSettings.getRetrySettings())) {
+    if (areRetriesDisabled(callSettings.getRetryableCodes())) {
       return innerCallable;
     }
 
@@ -218,8 +217,7 @@ public class Callables {
         initialCallable, scheduler, longRunningClient, operationCallSettings);
   }
 
-  private static boolean areRetriesDisabled(
-      Collection<StatusCode.Code> retryableCodes, RetrySettings retrySettings) {
-    return retrySettings.getMaxAttempts() == 1 || retryableCodes.isEmpty();
+  private static boolean areRetriesDisabled(Collection<StatusCode.Code> retryableCodes) {
+    return retryableCodes.isEmpty();
   }
 }
