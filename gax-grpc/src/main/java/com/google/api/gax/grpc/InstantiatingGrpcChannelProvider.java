921265a9983fa5e63ca0402dd41868ebb87e4e5b
Noah Dietz <ndietz@google.com>
2019-06-05 11:04:21 -0700

    Disable gRPC ServiceConfig look up

--- gax-grpc/src/main/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProvider.java
+++ gax-grpc/src/main/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProvider.java
@@ -229,6 +229,7 @@ public final class InstantiatingGrpcChannelProvider implements TransportChannelP
     }
     builder =
         builder
+            .disableServiceConfigLookUp()
             .intercept(new GrpcChannelUUIDInterceptor())
             .intercept(headerInterceptor)
             .intercept(metadataHandlerInterceptor)
