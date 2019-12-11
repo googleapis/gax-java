110f6a01f62a550c354103f38d99db2e91d9c81a
Cody Oss <codyoss@google.com>
2019-12-11 11:06:14 -0700

    fix formatting

--- gax/src/main/java/com/google/api/gax/rpc/ApiClientHeaderProvider.java
+++ gax/src/main/java/com/google/api/gax/rpc/ApiClientHeaderProvider.java
@@ -179,16 +179,12 @@ public class ApiClientHeaderProvider implements HeaderProvider, Serializable {
       return this;
     }
 
-    /**
-     * @return the quotaProjectIdToken used for quota and billing purposes.
-     */
+    /** @return the quotaProjectIdToken used for quota and billing purposes. */
     public String getQuotaProjectIdToken() {
       return quotaProjectIdToken;
     }
 
-    /**
-     * Sets the project ID used for quota and billing purposes.
-     */
+    /** Sets the project ID used for quota and billing purposes. */
     public Builder setQuotaProjectIdToken(String quotaProjectIdToken) {
       this.quotaProjectIdToken = quotaProjectIdToken;
       return this;
