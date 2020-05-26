28ec963300b4c569d006f63a936b2349f180874f
Tomo Suzuki <suztomo@google.com>
2020-05-26 12:15:15 -0400

    ci: Linkage Checker to examine linkage error at artifact level (#1032)
    * test: testing linkge checker with Gradle
    
    * linkage checker
    
    * Linkage Checker for gax, gax-grpc, gax-httpjson
    
    * Added in kokoro shell script
    
    * Removed unnecessary change
    
    * Reverting unnecessary change
    
    * Added comment for exclusion file
    
    * Exclusion file only for gax-grpc

--- .kokoro/linkage-monitor.sh
+++ .kokoro/linkage-monitor.sh
@@ -25,6 +25,7 @@ cd ${scriptDir}/..
 java -version
 echo ${JOB_TYPE}
 
+# Linkage Monitor checks the compatibility of GAX BOM
 # Using Gradle's maven-publish plugin. No need to sign the JAR.
 ./gradlew build publishToMavenLocal -x test -x signMavenJavaPublication
 
@@ -34,3 +35,7 @@ curl -v -O "https://storage.googleapis.com/cloud-opensource-java-linkage-monitor
 
 # Fails if there's new linkage errors compared with baseline
 java -jar ${JAR} com.google.cloud:libraries-bom
+
+
+# Linkage Checker looks for linkage errors at artifact level: gax, gax-grpc, and gax-httpjson
+./gradlew checkJavaLinkage
