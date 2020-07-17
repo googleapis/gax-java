0a8d1792ac39bd7c7297be6e7d4fe08ceef60de1
summerji <summerji@google.com>
2020-07-16 19:54:11 -0700

    Add unit test for QuotaProjectIdHidingCredentials

--- gax/src/test/java/com/google/api/gax/rpc/internal/QuotaProjectIdHidingCredentialsTest.java
+++ gax/src/test/java/com/google/api/gax/rpc/internal/QuotaProjectIdHidingCredentialsTest.java
@@ -1,76 +1,105 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google LLC nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.google.api.gax.rpc.internal;
 
-import static org.junit.Assert.fail;
-
+import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
-import java.io.ByteArrayInputStream;
+import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
 
 @RunWith(JUnit4.class)
 public class QuotaProjectIdHidingCredentialsTest {
   private static final String QUOTA_PROJECT_ID_KEY = "x-goog-user-project";
   private static final String QUOTA_PROJECT_ID_FROM_CREDENTIALS_VALUE =
       "quota_project_id_from_credentials";
-  private static final String JSON_KEY_QUOTA_PROJECT_ID =
-      "{\n"
-          + "  \"private_key_id\": \"somekeyid\",\n"
-          + "  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggS"
-          + "kAgEAAoIBAQC+K2hSuFpAdrJI\\nnCgcDz2M7t7bjdlsadsasad+fvRSW6TjNQZ3p5LLQY1kSZRqBqylRkzteMOyHg"
-          + "aR\\n0Pmxh3ILCND5men43j3h4eDbrhQBuxfEMalkG92sL+PNQSETY2tnvXryOvmBRwa/\\nQP/9dJfIkIDJ9Fw9N4"
-          + "Bhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nknddadwkwewcVxHFhcZJO+XWf6ofLUXpRwiTZakGMn8EE1uVa2"
-          + "LgczOjwWHGi99MFjxSer5m9\\n1tCa3/KEGKiS/YL71JvjwX3mb+cewlkcmweBKZHM2JPTk0ZednFSpVZMtycjkbLa"
-          + "\\ndYOS8V85AgMBewECggEBAKksaldajfDZDV6nGqbFjMiizAKJolr/M3OQw16K6o3/\\n0S31xIe3sSlgW0+UbYlF"
-          + "4U8KifhManD1apVSC3csafaspP4RZUHFhtBywLO9pR5c\\nr6S5aLp+gPWFyIp1pfXbWGvc5VY/v9x7ya1VEa6rXvL"
-          + "sKupSeWAW4tMj3eo/64ge\\nsdaceaLYw52KeBYiT6+vpsnYrEkAHO1fF/LavbLLOFJmFTMxmsNaG0tuiJHgjshB\\"
-          + "n82DpMCbXG9YcCgI/DbzuIjsdj2JC1cascSP//3PmefWysucBQe7Jryb6NQtASmnv\\nCdDw/0jmZTEjpe4S1lxfHp"
-          + "lAhHFtdgYTvyYtaLZiVVkCgYEA8eVpof2rceecw/I6\\n5ng1q3Hl2usdWV/4mZMvR0fOemacLLfocX6IYxT1zA1FF"
-          + "JlbXSRsJMf/Qq39mOR2\\nSpW+hr4jCoHeRVYLgsbggtrevGmILAlNoqCMpGZ6vDmJpq6ECV9olliDvpPgWOP+\\nm"
-          + "YPDreFBGxWvQrADNbRt2dmGsrsCgYEAyUHqB2wvJHFqdmeBsaacewzV8x9WgmeX\\ngUIi9REwXlGDW0Mz50dxpxcK"
-          + "CAYn65+7TCnY5O/jmL0VRxU1J2mSWyWTo1C+17L0\\n3fUqjxL1pkefwecxwecvC+gFFYdJ4CQ/MHHXU81Lwl1iWdF"
-          + "Cd2UoGddYaOF+KNeM\\nHC7cmqra+JsCgYEAlUNywzq8nUg7282E+uICfCB0LfwejuymR93CtsFgb7cRd6ak\\nECR"
-          + "8FGfCpH8ruWJINllbQfcHVCX47ndLZwqv3oVFKh6pAS/vVI4dpOepP8++7y1u\\ncoOvtreXCX6XqfrWDtKIvv0vjl"
-          + "HBhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nkndj5uNl5SiuVxHFhcZJO+XWf6ofLUregtevZakGMn8EE1uVa"
-          + "2AY7eafmoU/nZPT\\n00YB0TBATdCbn/nBSuKDESkhSg9s2GEKQZG5hBmL5uCMfo09z3SfxZIhJdlerreP\\nJ7gSi"
-          + "dI12N+EZxYd4xIJh/HFDgp7RRO87f+WJkofMQKBgGTnClK1VMaCRbJZPriw\\nEfeFCoOX75MxKwXs6xgrw4W//AYG"
-          + "GUjDt83lD6AZP6tws7gJ2IwY/qP7+lyhjEqN\\nHtfPZRGFkGZsdaksdlaksd323423d+15/UvrlRSFPNj1tWQmNKk"
-          + "XyRDW4IG1Oa2p\\nrALStNBx5Y9t0/LQnFI4w3aG\\n-----END PRIVATE KEY-----\\n\",\n"
-          + "  \"project_id\": \"someprojectid\",\n"
-          + "  \"client_email\": \"someclientid@developer.gserviceaccount.com\",\n"
-          + "  \"client_id\": \"someclientid.apps.googleusercontent.com\",\n"
-          + "  \"type\": \"service_account\",\n"
-          + "  \"quota_project_id\": \""
-          + QUOTA_PROJECT_ID_FROM_CREDENTIALS_VALUE
-          + "\"\n"
-          + "}";
-  private GoogleCredentials credentialsWithQuotaProjectId;
 
-  @Before
-  public void setup() {
-    credentialsWithQuotaProjectId =
-        loadCredentials(JSON_KEY_QUOTA_PROJECT_ID).createScoped("test_scope");
+  @Test
+  public void quotaProjectIdHidingCredentials_getRequestMetadata() throws IOException {
+    // Credentials with quota project id
+    Map<String, List<String>> metaDataWithQuota =
+        ImmutableMap.of(
+            "k1",
+            Collections.singletonList("v1"),
+            QUOTA_PROJECT_ID_KEY,
+            Collections.singletonList(QUOTA_PROJECT_ID_FROM_CREDENTIALS_VALUE));
+    Credentials credentialsWithQuotaProjectId = Mockito.mock(GoogleCredentials.class);
+    Mockito.when(credentialsWithQuotaProjectId.getRequestMetadata(null))
+        .thenReturn(metaDataWithQuota);
+    QuotaProjectIdHidingCredentials quotaProjectIdHidingCredentials =
+        new QuotaProjectIdHidingCredentials(credentialsWithQuotaProjectId);
+    Map<String, List<String>> metaDataHidingQuota =
+        quotaProjectIdHidingCredentials.getRequestMetadata();
+
+    Assert.assertTrue(metaDataWithQuota.containsKey(QUOTA_PROJECT_ID_KEY));
+    Assert.assertFalse(metaDataHidingQuota.containsKey(QUOTA_PROJECT_ID_KEY));
+    Assert.assertEquals(metaDataWithQuota.size() - 1, metaDataHidingQuota.size());
+
+    // Credentials without quota project id
+    Map<String, List<String>> metaDataWithoutQuota =
+        ImmutableMap.of("k1", Collections.singletonList("v1"));
+    Credentials credentialsWithoutQuotaProjectId = Mockito.mock(GoogleCredentials.class);
+    Mockito.when(credentialsWithoutQuotaProjectId.getRequestMetadata(null))
+        .thenReturn(metaDataWithoutQuota);
+    QuotaProjectIdHidingCredentials quotaProjectIdHidingCredentialsWithout =
+        new QuotaProjectIdHidingCredentials(credentialsWithoutQuotaProjectId);
+    Map<String, List<String>> metaDataHidingQuotaWithout =
+        quotaProjectIdHidingCredentials.getRequestMetadata();
+
+    Assert.assertEquals(
+        quotaProjectIdHidingCredentialsWithout.getRequestMetadata(), metaDataWithoutQuota);
   }
 
   @Test
-  public void createQuotaProjectIdHidingCredentials() throws IOException {
-    QuotaProjectIdHidingCredentials quotaProjectIdHidingCredentials =
-        new QuotaProjectIdHidingCredentials(credentialsWithQuotaProjectId);
-    Assert.assertFalse(
-        quotaProjectIdHidingCredentials.getRequestMetadata().containsKey(QUOTA_PROJECT_ID_KEY));
-  }
+  public void quotaProjectIdHidingCredentials_getAuthenticationType() throws IOException {
+    final String mockType = "mock_type";
+    Credentials credentials = Mockito.mock(GoogleCredentials.class);
+    Mockito.when(credentials.getAuthenticationType()).thenReturn(mockType);
+    Mockito.when(credentials.hasRequestMetadata()).thenReturn(true);
+    Mockito.when(credentials.hasRequestMetadataOnly()).thenReturn(false);
 
-  static GoogleCredentials loadCredentials(String credentialFile) {
-    try {
-      InputStream keyStream = new ByteArrayInputStream(credentialFile.getBytes());
-      return GoogleCredentials.fromStream(keyStream);
-    } catch (IOException e) {
-      fail("Couldn't create fake JSON credentials.");
-    }
-    return null;
+    QuotaProjectIdHidingCredentials quotaProjectIdHidingCredentials =
+        new QuotaProjectIdHidingCredentials(credentials);
+    quotaProjectIdHidingCredentials.refresh();
+
+    Assert.assertEquals(quotaProjectIdHidingCredentials.getAuthenticationType(), mockType);
+    Assert.assertTrue(quotaProjectIdHidingCredentials.hasRequestMetadata());
+    Assert.assertFalse(quotaProjectIdHidingCredentials.hasRequestMetadataOnly());
+
+    Mockito.verify(credentials, Mockito.atLeastOnce()).refresh();
   }
 }
