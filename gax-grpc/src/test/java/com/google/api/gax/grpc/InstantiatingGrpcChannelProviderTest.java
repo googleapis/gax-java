950df69c0f7aed2a63e59cc46dbc4fa805c73722
Penn (Dapeng) Zhang <zdapeng@google.com>
2020-12-10 10:37:02 -0800

    Revert "fix 'add test with branch coverage'"
    This reverts commit 934830f20daf844cfdaae8bd2354ae108761e198.

--- gax-grpc/src/test/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProviderTest.java
+++ gax-grpc/src/test/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProviderTest.java
@@ -396,8 +396,6 @@ public class InstantiatingGrpcChannelProviderTest {
 
       InstantiatingGrpcChannelProvider provider =
           InstantiatingGrpcChannelProvider.newBuilder()
-              .setAttemptDirectPath(true)
-              .setCredentials(ComputeEngineCredentials.create())
               .setEndpoint("localhost:8080")
               .setPoolSize(poolSize)
               .setHeaderProvider(Mockito.mock(HeaderProvider.class))
