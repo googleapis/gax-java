cfe12b91647ba73b89747c6bcaf0627f66c1ba4f
Mattie Fu <mattiefu@google.com>
2021-03-05 15:41:54 +0000

    fix comment

--- gax/src/main/java/com/google/api/gax/batching/NonBlockingSemaphore.java
+++ gax/src/main/java/com/google/api/gax/batching/NonBlockingSemaphore.java
@@ -50,9 +50,9 @@ class NonBlockingSemaphore implements Semaphore64 {
   public synchronized void release(long permits) {
     checkNotNegative(permits);
 
+    currentPermits += permits;
     // If more permits are returned than what was originally set, we need to add these extra
     // permits to the limit
-    currentPermits += permits;
     if (currentPermits > limit) {
       limit = currentPermits;
     }
