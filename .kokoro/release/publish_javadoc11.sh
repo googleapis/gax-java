8fc92e74e7919304b9dc5a88ad001a10e921de32
Emily Ball <emilyball@google.com>
2021-05-19 09:40:30 -0700

    chore: update cloud-rad doclet
    Updates cloud rad doc generation to use new doclet. Added the new doclet jar to cloud-devrel-kokoro-resources/docfx bucket

--- .kokoro/release/publish_javadoc11.sh
+++ .kokoro/release/publish_javadoc11.sh
@@ -36,10 +36,13 @@ VERSION=$(grep ${NAME}: versions.txt | cut -d: -f3)
 # build the docs
 ./gradlew javadocCombinedV3
 
-# copy README to tmp_docs dir and rename index.md
-cp README.md tmp_docs/index.md
+# copy README to docfx-yml dir and rename index.md
+cp README.md tmp_docs/docfx-yml/index.md
 
-pushd tmp_docs
+# copy CHANGELOG to docfx-yml dir and rename history.md
+cp CHANGELOG.md tmp_docs/docfx-yml/history.md
+
+pushd tmp_docs/docfx-yml/
 
 # create metadata
 python3 -m docuploader create-metadata \
