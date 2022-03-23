9f99723605da691b3ab37d79bffbf99ec2ebc1cc
Blake Li <blakeli@google.com>
2022-03-23 17:04:36 -0400

    Make ErrorDetails final in ApiException

--- gax/src/main/java/com/google/api/gax/rpc/ApiException.java
+++ gax/src/main/java/com/google/api/gax/rpc/ApiException.java
@@ -37,25 +37,26 @@ public class ApiException extends RuntimeException {
 
   private static final long serialVersionUID = -4375114339928877996L;
 
-  private ErrorDetails errorDetails;
+  private final ErrorDetails errorDetails;
   private final StatusCode statusCode;
   private final boolean retryable;
 
   public ApiException(Throwable cause, StatusCode statusCode, boolean retryable) {
-    super(cause);
-    this.statusCode = Preconditions.checkNotNull(statusCode);
-    this.retryable = retryable;
+    this(cause, statusCode, retryable, null);
   }
 
   public ApiException(String message, Throwable cause, StatusCode statusCode, boolean retryable) {
     super(message, cause);
     this.statusCode = Preconditions.checkNotNull(statusCode);
     this.retryable = retryable;
+    this.errorDetails = null;
   }
 
   public ApiException(
       Throwable cause, StatusCode statusCode, boolean retryable, ErrorDetails errorDetails) {
-    this(cause, statusCode, retryable);
+    super(cause);
+    this.statusCode = Preconditions.checkNotNull(statusCode);
+    this.retryable = retryable;
     this.errorDetails = errorDetails;
   }
 
