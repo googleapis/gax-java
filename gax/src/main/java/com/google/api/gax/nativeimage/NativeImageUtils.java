9b863be8bb09877b39a35547008e59968ed87fbe
Mridula Peddada <mpeddada@google.com>
2022-03-25 10:12:03 -0400

    chore: relocate Netty Native Image configurations from java-core to gax

---
+++ gax/src/main/java/com/google/api/gax/nativeimage/NativeImageUtils.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google LLC nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.google.api.gax.nativeimage;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.logging.Logger;
+import org.graalvm.nativeimage.hosted.Feature.FeatureAccess;
+import org.graalvm.nativeimage.hosted.RuntimeReflection;
+
+/** Internal class offering helper methods for registering methods/classes for reflection. */
+public class NativeImageUtils {
+
+  private static final Logger LOGGER = Logger.getLogger(NativeImageUtils.class.getName());
+
+  /** Returns the method of a class or fails if it is not present. */
+  public static Method getMethodOrFail(Class<?> clazz, String methodName, Class<?>... params) {
+    try {
+      return clazz.getDeclaredMethod(methodName, params);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(
+          "Failed to find method " + methodName + " for class " + clazz.getName(), e);
+    }
+  }
+
+  /** Registers a class for reflective construction via its default constructor. */
+  public static void registerForReflectiveInstantiation(FeatureAccess access, String className) {
+    Class<?> clazz = access.findClassByName(className);
+    if (clazz != null) {
+      RuntimeReflection.register(clazz);
+      RuntimeReflection.registerForReflectiveInstantiation(clazz);
+    } else {
+      LOGGER.warning(
+          "Failed to find " + className + " on the classpath for reflective instantiation.");
+    }
+  }
+
+  /** Registers all constructors of a class for reflection. */
+  public static void registerConstructorsForReflection(FeatureAccess access, String name) {
+    Class<?> clazz = access.findClassByName(name);
+    if (clazz != null) {
+      RuntimeReflection.register(clazz);
+      RuntimeReflection.register(clazz.getDeclaredConstructors());
+    } else {
+      LOGGER.warning("Failed to find " + name + " on the classpath for reflection.");
+    }
+  }
+
+  /** Registers an entire class for reflection use. */
+  public static void registerClassForReflection(FeatureAccess access, String name) {
+    Class<?> clazz = access.findClassByName(name);
+    if (clazz != null) {
+      RuntimeReflection.register(clazz);
+      RuntimeReflection.register(clazz.getDeclaredConstructors());
+      RuntimeReflection.register(clazz.getDeclaredFields());
+      RuntimeReflection.register(clazz.getDeclaredMethods());
+    } else {
+      LOGGER.warning("Failed to find " + name + " on the classpath for reflection.");
+    }
+  }
+
+  /**
+   * Registers the transitive class hierarchy of the provided {@code className} for reflection.
+   *
+   * <p>The transitive class hierarchy contains the class itself and its transitive set of
+   * *non-private* nested subclasses.
+   */
+  public static void registerClassHierarchyForReflection(FeatureAccess access, String className) {
+    Class<?> clazz = access.findClassByName(className);
+    if (clazz != null) {
+      registerClassForReflection(access, className);
+      for (Class<?> nestedClass : clazz.getDeclaredClasses()) {
+        if (!Modifier.isPrivate(nestedClass.getModifiers())) {
+          registerClassHierarchyForReflection(access, nestedClass.getName());
+        }
+      }
+    } else {
+      LOGGER.warning("Failed to find " + className + " on the classpath for reflection.");
+    }
+  }
+
+  /** Registers a class for unsafe reflective field access. */
+  public static void registerForUnsafeFieldAccess(
+      FeatureAccess access, String className, String... fields) {
+    Class<?> clazz = access.findClassByName(className);
+    if (clazz != null) {
+      RuntimeReflection.register(clazz);
+      for (String fieldName : fields) {
+        try {
+          RuntimeReflection.register(clazz.getDeclaredField(fieldName));
+        } catch (NoSuchFieldException ex) {
+          LOGGER.warning("Failed to register field " + fieldName + " for class " + className);
+          LOGGER.warning(ex.getMessage());
+        }
+      }
+    } else {
+      LOGGER.warning(
+          "Failed to find "
+              + className
+              + " on the classpath for unsafe fields access registration.");
+    }
+  }
+
+  /** Registers all the classes under the specified package for reflection. */
+  public static void registerPackageForReflection(FeatureAccess access, String packageName) {
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+    try {
+      String path = packageName.replace('.', '/');
+
+      Enumeration<URL> resources = classLoader.getResources(path);
+      while (resources.hasMoreElements()) {
+        URL url = resources.nextElement();
+
+        URLConnection connection = url.openConnection();
+        if (connection instanceof JarURLConnection) {
+          List<String> classes = findClassesInJar((JarURLConnection) connection, packageName);
+          for (String className : classes) {
+            registerClassHierarchyForReflection(access, className);
+          }
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to load classes under package name.", e);
+    }
+  }
+
+  private static List<String> findClassesInJar(JarURLConnection urlConnection, String packageName)
+      throws IOException {
+
+    List<String> result = new ArrayList<>();
+
+    final JarFile jarFile = urlConnection.getJarFile();
+    final Enumeration<JarEntry> entries = jarFile.entries();
+
+    while (entries.hasMoreElements()) {
+      JarEntry entry = entries.nextElement();
+      String entryName = entry.getName();
+
+      if (entryName.endsWith(".class")) {
+        String javaClassName = entryName.replace(".class", "").replace('/', '.');
+
+        if (javaClassName.startsWith(packageName)) {
+          result.add(javaClassName);
+        }
+      }
+    }
+
+    return result;
+  }
+}
